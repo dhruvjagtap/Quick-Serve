7ba34e10f9a70df6ff8cfb5c1e200a7968608258
dhruvjagtap <jagtapdhruv6@gmail.com>
2025-04-08 16:31:32 +0530

    Created SplashScreen (App Loading screen)

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,48 @@
+
+
+import React, { useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Animated } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/AppNavigator'; // adjust path if needed
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>;
+
+const SplashScreen = ({ navigation }: Props) => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+    }).start(() => {
+      setTimeout(() => {
+        navigation.replace('Welcome');
+      }, 1000);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.Text style={[styles.logoText, { opacity }]}>
+        QuickServe
+      </Animated.Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logoText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#4B7BE5',
+  },
+});
+
+export default SplashScreen;
